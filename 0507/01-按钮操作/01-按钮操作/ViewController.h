37350c640911c8e85fa4f02778e047b8657c9a21
Sophia Tang <sophia.soduku@gmail.com>
2015-05-11 21:40:24 +0800

    modified

---
+++ 0507/01-按钮操作/01-按钮操作/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  01-按钮操作
+//
+//  Created by Sophia Tang on 15/5/7.
+//  Copyright (c) 2015年 Sophia Tang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+- (IBAction)up:(id)sender;
+
+
+
+@end
+
---
+++ 0507/01-按钮操作/01-按钮操作/ViewController.m
@@ -0,0 +1,33 @@
+//
+//  ViewController.m
+//  01-按钮操作
+//
+//  Created by Sophia Tang on 15/5/7.
+//  Copyright (c) 2015年 Sophia Tang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+- (IBAction)up:(id)sender
+{
+    NSLog(@"up");
+    
+}
+@end
